f3558644a8d47e32a5538a6845c6a80e72668042
tony(kitchen) <tonetheman@gmail.com>
2023-12-01 16:00:39 -0600

    not working i need to learn to read

--- day1/prog.py
+++ day1/prog.py
@@ -28,4 +28,110 @@ def part1():
     print("part1",res)    
     
     
-part1()
+# part1()
+
+def part2():
+    inf = open("sample2.txt","r")
+    lines = inf.readlines()
+    inf.close()
+
+    res = 0
+    for data in lines:
+        first = True
+        ts = ""
+        for i in range(len(data)):
+            c = data[i]
+            if c in ['o','t','f','s','e','n']:
+                # check for case where digit is spelled
+                if data[i:].startswith("two"):
+                    if first:
+                        ts = "2"
+                        first = False
+                elif data[i:].startswith("one"):
+                    if first:
+                        ts = "1"
+                        first = False
+                elif data[i:].startswith("three"):
+                    if first:
+                        ts = "3"
+                        first = False
+                elif data[i:].startswith("four"):
+                    if first:
+                        ts = "4"
+                        first = False
+                elif data[i:].startswith("five"):
+                    if first:
+                        ts = "5"
+                        first = False
+                elif data[i:].startswith("six"):
+                    if first:
+                        ts = "6"
+                        first = False
+                elif data[i:].startswith("seven"):
+                    if first:
+                        ts = "7"
+                        first = False
+                elif data[i:].startswith("eight"):
+                    if first:
+                        ts = "8"
+                        first = False
+                elif data[i:].startswith("nine"):
+                    if first:
+                        ts = "9"
+                        first = False
+            elif c in ['0','1','2','3','4','5','6','7','8','9']:
+                if first:
+                    ts = str(c)
+                    first = False       
+        first = True
+        rdata = data[::-1]
+        for i in range(len(rdata)):
+            c = rdata[i]
+            if c in ['o','t','f','s','e','n']:
+                # check for case where digit is spelled
+                if data[i:].startswith("two"):
+                    if first:
+                        ts = ts + "2"
+                        first = False
+                elif data[i:].startswith("one"):
+                    if first:
+                        ts = ts + "1"
+                        first = False
+                elif data[i:].startswith("three"):
+                    if first:
+                        ts = ts + "3"
+                        first = False
+                elif data[i:].startswith("four"):
+                    if first:
+                        ts = ts + "4"
+                        first = False
+                elif data[i:].startswith("five"):
+                    if first:
+                        ts = ts + "5"
+                        first = False
+                elif data[i:].startswith("six"):
+                    if first:
+                        ts = ts + "6"
+                        first = False
+                elif data[i:].startswith("seven"):
+                    if first:
+                        ts = ts + "7"
+                        first = False
+                elif data[i:].startswith("eight"):
+                    if first:
+                        ts = ts + "8"
+                        first = False
+                elif data[i:].startswith("nine"):
+                    if first:
+                        ts = ts + "9"
+                        first = False
+            elif c in ['0','1','2','3','4','5','6','7','8','9']:
+                if first:
+                    ts = ts + str(c)
+                    first = False
+
+        print(ts)
+        res = res + int(ts)
+    print("res",res)
+
+part2()
