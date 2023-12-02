77b9364cd2352aedac4a0b9b63c950f594d126c3
tony <tonetheman@gmail.com>
2023-12-02 11:38:17 -0600

    saving

--- day2/prog.py
+++ day2/prog.py
@@ -1,12 +1,4 @@
-
-class GameData:
-    color:str
-    count:int
-
-class Game:
-    id : int
-    data : "list[GameData]"
-
+#!/usr/bin/python3
 
 inf = open("sample1.txt","r")
 data = inf.readlines()
@@ -18,25 +10,41 @@ def parse_a_line(line:str):
     print(line)
     print(parts)
 
+    # get the game number first prob is not important
     game_number = int(parts[0].split(" ")[1])
     gdata = parts[1].rstrip().lstrip()
+    
+    # need something to hold the rounds
     gdata_list = []
 
+    # split on semi into rounds
     parts = gdata.split(";")
     print(parts)
+
     for p in parts:
         p = p.lstrip().rstrip()
         print("work on",p)
         small_parts = p.split(",")
         print(small_parts)
+        tmp = []
         for sp in small_parts:
             sp = sp.lstrip().rstrip()
             spdata = sp.split(" ")
             print("spdata:",spdata)
-            gdata_list.append((int(spdata[0]),spdata[1]))
+            tmp.append((int(spdata[0]),spdata[1]))
+        gdata_list.append(tmp)
 
     print("game number",game_number)
     print(gdata_list)
     print()
 
-parse_a_line(data[0])
+    class Holder:
+        def __init__(self,game_number,data):
+            self.game_number = game_number
+            self.data = data
+        def __repr__(self) -> str:
+            return str(self.game_number) + ";;" + str(self.data)
+    return Holder(game_number,gdata_list)
+
+res = parse_a_line(data[0])
+print(res)
