b90a95320d45ab87e8e17df5e9da8228fe5d23e4
tonethemna <tonetheman@gmail.com>
2023-12-02 07:46:43 -0600

    saving parsing is coming along

--- day2/prog.py
+++ day2/prog.py
@@ -1,18 +1,32 @@
+
+from dataclasses import dataclass
+
+@dataclass
+class Game:
+    game_number:int
+
 inf = open("sample1.txt","r")
 data = inf.readlines()
 inf.close()
 
-
 def parse_a_line(line:str):
-    parts = line.split(" ")
+    parts = line.split(":")
 
     print(line)
     print(parts)
 
-    game_number = int(parts[1].split(":")[0])
+    game_number = int(parts[0].split(" ")[1])
+    gdata = parts[1].rstrip().lstrip()
+
+    parts = gdata.split(";")
+    print(parts)
+    for p in parts:
+        p = p.lstrip().rstrip()
+        print("work on",p)
+        small_parts = p.split(",")
+        print(small_parts)
 
     print("game number",game_number)
-
     print()
 
 parse_a_line(data[0])
