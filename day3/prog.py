f7239eb1c9ef7595335816038a49b5e4f45461fe
tony <tonetheman@gmail.com>
2023-12-04 20:09:14 -0600

    new version still not fucking working

--- day3/prog.py
+++ day3/prog.py
@@ -1,125 +1,88 @@
 #!/usr/bin/python3
 
-inf = open("input.txt","r")
+from typing import List
+
+
+inf = open("sample.txt","r")
 data = inf.readlines()
 inf.close()
 
-groups = []
-
-
-def is_part(v):
-    return v in ['0','1','2','3','4','5','6','7','8','9']
-
-def pg(g):
-    for line in g:
-        print(line)
-
-def make_grid(data):
-    grid = []
-    for line in data:
-        line=line.rstrip()
-        tmp = []
-        for c in line:
-            if c=='.':
-                tmp.append('.')
-            elif is_part(c):
+class Grid:
+    def __init__(self,data):
+        self.grid = []
+        for line in data:
+            line = line.rstrip()
+            tmp = []
+            for c in line:
                 tmp.append(c)
-            else:
-                tmp.append(c)
-        grid.append(tmp)
-    return grid
-
-
-def look_for_symbol(grid,r,c):
-    
-    def get_val(_r,_c):
+            self.grid.append(tmp)
+    def __repr__(self):
+        ts = ""
+        for row in self.grid:
+            for c in row:
+                ts = ts + str(c)
+            ts = ts + "\n"
+        return ts
+    def get(self,r,c):
         try:
-            tmp = grid[_r][_c]
+            return self.grid[r][c]
         except IndexError:
             return '.'
-        return tmp
-    
-    def check(_v):
-        if _v=='.' or is_part(_v):
-            return False
-        return True
-
-    # diag
-    tmp = get_val(r+1,c+1) # diag front right
-    if check(tmp):
-        return True
-    tmp = get_val(r-1,c-1) # diag back left
-    if check(tmp):
-        return True
-    tmp = get_val(r-1,c+1)
-    if check(tmp):
-        return True
-    tmp = get_val(r+1,c-1)
-    if check(tmp):
-        return True
-    
-    # ahead
-    tmp = get_val(r,c+1)
-    if check(tmp):
-        return True
-    # behind
-    tmp = get_val(r,c-1)
-    if check(tmp):
-        return True
-    # up
-    tmp = get_val(r-1,c)
-    if check(tmp):
-        return True
-    # down
-    tmp = get_val(r+1,c)
-    if check(tmp):
-        return True
-    
-    return False
-
-groups = {}
-current_group = ""
-in_group = False
-has_sym = False
-grid = make_grid(data)
-pg(grid)
-
-for i in range(len(grid)): # rows
-    for j in range(len(grid[0])): # cols
-        if is_part(grid[i][j]):
-            if in_group:
-                current_group = current_group + str(grid[i][j])
-                if look_for_symbol(grid,i,j):
-                    has_sym = True
-            else:
-                # make a new group
-                in_group = True
-                current_group = str(grid[i][j])
-                if look_for_symbol(grid,i,j):
-                    has_sym = True
-        else:
-            if current_group=="":
-                pass
-            else:
-                if not current_group in groups:
-                    groups[current_group] = 0
-
-            if has_sym:
-                print("this group had a sym",current_group)
-                if (not groups[current_group]):
-                    groups[current_group] = 1
-                else:
-                    groups[current_group] += 1
-
-            # not a part reset group
-            current_group = ""
-            in_group = False
-            has_sym = False
         
-        # print("current group",current_group)
+    def check(self,r,c):
+        ii = [
+            (-1,-1),
+            (-1,0),
+            (-1,1),
+            (0,-1),
+            (0,1),
+            (1,-1),
+            (1,0),
+            (1,1)
+        ]
+        for (ri,ci) in ii:
+            computed_r = r + ri
+            computed_c = c + ci
+            print(computed_r,computed_c)
+            if computed_c<0 or computed_r<0:
+                pass # ignore
+            else:
+                v = self.get(computed_r,computed_c)
+                if not v in ['.','0','1','2','3','4','5','6','7','8','9']:
+                    return True
+        return False
 
-total = 0
-for k in groups:
-    if groups[k] > 0:
-        total = total + groups[k] * int(k)
-print(total)
+grid = Grid(data)
+groups = []
+
+rindex = 0
+for line in grid.grid:
+    cindex = 0
+    in_group = False
+    current_group = ""
+    current_group_status = False
+    for c in line:
+        if c in ['0','1','2','3','4','5','6','7','8','9']:
+            if in_group:
+                current_group = current_group + str(c)
+            else:
+                in_group = True
+                current_group = current_group + str(c)
+            if grid.check(rindex,cindex):
+                if current_group_status==False:
+                    current_group_status = True
+        elif c=='.':
+            if in_group:
+
+                print("not in group anymore",current_group_status,current_group)
+                # no longer in a group
+                in_group = False
+                current_group = ""
+                current_group_status = False
+        print(current_group)
+    
+    if in_group:
+        print("in group at end of line")
+
+        cindex += 1
+    rindex += 1
