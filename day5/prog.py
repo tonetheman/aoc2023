54c0f8989defbd2ee65a5a74bf6a0a10136e8867
tony <tonetheman@gmail.com>
2023-12-05 20:41:40 -0600

    still mem hog

--- day5/prog.py
+++ day5/prog.py
@@ -32,6 +32,8 @@ HUMID_TO_LOC = 7
 state = START
 
 seed_to_soil = []
+seed_to_soil_m = {}
+
 soil_to_fert = []
 fert_to_water = []
 water_to_light = []
@@ -152,9 +154,134 @@ def part2():
 
     print("low",lowest)
 
+c1 = {}
+def find_num1(target, iranges):
+    if target in c1: return c1[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c1[target] = r.dest + adjust
+            return r.dest + adjust
+    c1[target] = target
+    return target
+c2 = {}
+def find_num2(target, iranges):
+    if target in c2: return c2[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c2[target] = r.dest + adjust
+            return r.dest + adjust
+    c2[target] = target
+    return target
+c3 = {}
+def find_num3(target, iranges):
+    if target in c3: return c3[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c3[target] = r.dest + adjust
+            return r.dest + adjust
+    c3[target] = target
+    return target
+c4={}
+def find_num4(target, iranges):
+    if target in c4: return c4[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c4[target] = r.dest  + adjust
+            return r.dest + adjust
+    c4[target] = target
+    return target
+c5={}
+def find_num5(target, iranges):
+    if target in c5: return c5[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c5[target] = r.dest + adjust
+            return r.dest + adjust
+    c5[target] = target
+    return target
+c6={}
+def find_num6(target, iranges):
+    if target in c6: return c6[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c6[target] = r.dest + adjust
+            return r.dest + adjust
+    c6[target] = target
+    return target
+c7={}
+def find_num7(target, iranges):
+    if target in c7: return c7[target]
+    for r in iranges:
+        if target >= r.src and target <= r.src+r.range:
+            # print("matched this range",r, "target",target)
+            adjust = target - r.src
+            # print("need to adjust",adjust,r.dest+adjust)
+            c7[target] = r.dest + adjust
+            return r.dest + adjust
+    c7[target] = target
+    return target
+
+# big cache
+cc={}
+
+def part2a():
+    lowest = 999999999
+
+    seeds = []
+    pos = 0
+    print("len",len(seed_nums))
+    while True:
+        starting = int(seed_nums[pos])
+        pos += 1
+        r = int(seed_nums[pos])
+        pos += 1
+        print("checking r",pos,r)
+        for i in range(r):
+            n = starting+i
+            if n in cc:
+                res6 = cc[n]
+                print("cc hit")
+            else:
+                res = find_num(n,seed_to_soil)
+                res1 = find_num(res,soil_to_fert)
+                res2 = find_num(res1,fert_to_water)
+                res3 = find_num(res2,water_to_light)
+                res4 = find_num(res3,light_to_temp)
+                res5 = find_num(res4,temp_to_humid)
+                res6 = find_num(res5,humid_to_loc)
+                cc[n] = res6
+            if res6<lowest:
+                lowest=res6
+
+
+        if pos == len(seed_nums):
+            break
+
+
+
+
+    print("low",lowest)
 
 
 
 
 
-part2()
+part2a()
