90de4f4e76ef02b7a3effd883f46106ec6bd5bbb
tony(kitchen) <tonetheman@gmail.com>
2023-12-19 17:00:34 -0600

    thinking

--- day19/prog.py
+++ day19/prog.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""
+eval could be interesting for this
+>>> eval("a",None,{"a":10})
+10
+>>> eval("a<200",None,{"a":10})
+True
+>>> eval("a>200",None,{"a":10})
+False
+"""
 
 def part1():
     inf = open("sample.txt", "r")
@@ -23,6 +32,8 @@ def part1():
             rule_name = p1[0]
             rest_of_rule = p1[1].split("}")
             rr = rest_of_rule[0].split(",")
+            for r in rr:
+                print("\t",r)
             print("rr",rr)
 
             # TODO parse out the rest of each rule
@@ -39,5 +50,42 @@ def part1():
             # switch parsing
             pass
 
+def junk():
+    class Rule:
+        def __init__(self,name):
+            self.name = name
+            self.rules = []
 
-part1()
+    r1 = Rule("in")
+    r1.rules.append({"match" : "s<1351",
+                    "target" : "px"})
+    r1.rules.append({"match" : "DEFAULT",
+                     "target":"qqz"})
+
+
+    def test_rule(r,x,m,a,s):
+        # do not dig this from all the dumb dict
+        # syntax
+        current_rule = 0
+        res = eval(r.rules[current_rule]["match"],None,
+                {"a":a,
+                 "x":x,
+                 "m":m,
+                 "s":s})
+        if res==False:
+            # move to next rule in this case
+            current_rule = current_rule + 1
+            if r.rules[current_rule]["match"] == "DEFAULT":
+                return r.rules[current_rule]["target"]
+        else:
+            # found a match bug out quick
+            return r.rules[current_rule]["target"]
+
+        
+        
+
+    print( test_rule(r1,787,2655,1222,2876) )
+
+junk()
+    
+
