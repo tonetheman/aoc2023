e323d86bec66b1689900f8aa854b6e99cad9958e
tonethemna <tonetheman@gmail.com>
2023-12-19 22:24:23 -0600

    further but not done think the data is filled out correct

--- day19/prog.py
+++ day19/prog.py
@@ -10,6 +10,55 @@ True
 False
 """
 
+from typing import Any
+
+
+class MatchRule:
+    def __init__(self,match,target):
+        self.match = match
+        self.target = target
+    def __repr__(self) -> str:
+        return str(self.match) + " " + str(self.target)
+
+class Rule:
+    def __init__(self,name):
+        self.name = name
+        self.rules = []
+    def add(self,ropts):
+        self.rules.append(ropts)
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Values:
+    def __init__(self,_s):
+        self._s = _s
+
+        self._s = self._s[1:]
+        
+        self._s = self._s[0:len(_s)-3]
+        tmp = self._s.split(",")
+        # interesting that each element in tmp
+        # looks like x = 10 or s = 20
+        # these can be evaluated too
+        # though not sure how to get the assignment
+        # to escape the eval
+        for t in tmp:
+            crud = t.split("=")
+            if crud[0]=='x':
+                self.x = int(crud[1])
+            elif crud[0]=='m':
+                self.m = int(crud[1])
+            elif crud[0]=='a':
+                self.a = int(crud[1])
+            elif crud[0]=='s':
+                self.s = int(crud[1])
+    def __repr__(self) -> str:
+        ts = "x=" + str(self.x)
+        ts += "m=" + str(self.m)
+        ts += "a=" + str(self.a)
+        ts += "s=" + str(self.s)
+        return ts
+    
 def part1():
     inf = open("sample.txt", "r")
     data= inf.readlines()
@@ -19,49 +68,49 @@ def part1():
     STATE_ONE = 1
 
     state = STATE_START
-
+    indexed_rules = {}
+    all_values = []
 
     def parse_line(line):
         nonlocal state
         if state == STATE_START:
             p1 = line.split("{")
-            print(p1)
             if p1[0]=="\n":
                 state = STATE_ONE
                 return False
             rule_name = p1[0]
+            cr = Rule(rule_name)
+            indexed_rules[rule_name] = cr # save a reference
             rest_of_rule = p1[1].split("}")
             rr = rest_of_rule[0].split(",")
             for r in rr:
-                print("\t",r)
-            print("rr",rr)
+                colon_index = r.find(":")
+                if colon_index!=-1:
+                    rs= r.split(":")
+                    if len(rs)==2:
+                        cr.add(MatchRule(rs[0],rs[1]))
+                else:
+                    cr.add(MatchRule("DEFAULT",r))
 
             # TODO parse out the rest of each rule
             # store somewhere hahaha
             
         elif state == STATE_ONE:
-            print("state one", line)
+            v = Values(line)
+            all_values.append(v)
 
         return True
 
     for line in data:
         res = parse_line(line)
-        if res==False:
-            # switch parsing
-            pass
+    
+    # all_values has stuff in it
+    v = values[0]
+
+
+
 
 def junk():
-    class MatchRule:
-        def __init__(self,match,target):
-            self.match = match
-            self.target = target
-
-    class Rule:
-        def __init__(self,name):
-            self.name = name
-            self.rules = []
-        def add(self,ropts):
-            self.rules.append(ropts)
 
     r1 = Rule("in")
     r1.add(MatchRule("s<1351","px"))
@@ -70,19 +119,29 @@ def junk():
     r2 = Rule("qqz")
     r2.add(MatchRule("s>2770","qs"))
     r2.add(MatchRule("m<1801","hdj"))
-    r2.add(MatchRule("TERM","R"))
+    r2.add(MatchRule("DEFAULT","R")) # not sure about match name
 
     r3 = Rule("qs")
-    r2.add(MatchRule("s>3448","A"))
-    r2.add(MatchRule("DEFAULT","lnx"))
-                     
+    r3.add(MatchRule("s>3448","A"))
+    r3.add(MatchRule("DEFAULT","lnx"))
+
+    r4= Rule("lnx")
+    r4.add(MatchRule("m>1548","A"))
+    r4.add(MatchRule("DEFAULT","A"))
+
+
     def test_rule(r,x,m,a,s):
         # do not dig this from all the dumb dict
         # syntax
         current_rule = 0
         while True:
-            print(r)
-            res = eval(r.rules[current_rule].match,None,
+            # print(r.name)
+            cr = r.rules[current_rule]
+            # print("\t",cr.match,cr.target)
+            if cr.match=="DEFAULT":
+                return cr.target # this could be A R or a legit target!
+            
+            res = eval(cr.match,None,
                     {"a":a,
                      "x":x,
                      "m":m,
@@ -90,8 +149,6 @@ def junk():
             if res==False:
                 # move to next rule in this case
                 current_rule = current_rule + 1
-                if r.rules[current_rule].match == "DEFAULT":
-                    return r.rules[current_rule].target
             else:
                 # found a match bug out quick
                 return r.rules[current_rule].target
@@ -102,10 +159,10 @@ def junk():
     print( test_rule(r1,787,2655,1222,2876) )
     print( test_rule(r2,787,2655,1222,2876) )
     print( test_rule(r3,787,2655,1222,2876) )
+    print( test_rule(r4,787,2655,1222,2876) )
 
 
 
-junk()
-
+part1()
 
 
